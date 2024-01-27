744a7c9b245403912d1a8a3f4ecb9761a497f4a3
Xeloini <Sparda042.vergil@gmail.com>
2024-01-27 05:26:09 +0100

    intégration des fleches du carousel et des dropdowns

--- App/kasa/src/components/Carousel/Carousel.jsx
+++ App/kasa/src/components/Carousel/Carousel.jsx
@@ -1,17 +1,12 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import classes from "./Carousel.module.scss";
+import left from '../../assets/ARROWLEFT.svg';
+import right from '../../assets/ARROWRIGHT.svg';
 
 export const Carousel = ({ data }) => {
 	const [currentIndex, setCurrentIndex] = useState(0);
 	const maxTemp = data.length;
-	const carouselInfiniteScroll = () => {
-		const maxTemp = data.length;
-		if (currentIndex === maxTemp-1) {
-			return setCurrentIndex(0);
-		}
-		return setCurrentIndex(currentIndex + 1)
-	}
 	const toNext = () => {
 		if (currentIndex === maxTemp-1) {
 			return setCurrentIndex(0);
@@ -24,25 +19,20 @@ export const Carousel = ({ data }) => {
 		}
 		return setCurrentIndex(currentIndex - 1)
 	}
-	useEffect(() => {
-		const interval = setInterval(() => { carouselInfiniteScroll() }, 3000)
-		return clearInterval(interval)
-	})
 
 	return (
-		<div className="carousel-control">
-		<button onClick={toPrev}>previous</button>
-		<img src="../../assets/ARROWLEFT.svg" alt="previous"></img>
-		<div className={classes.container}>
+		<div className={classes.control}>
+			<button onClick={toPrev} className={classes.prev}><img src={left} alt="previous"></img></button>
+			<div className={classes.container}>
 				{data.map((image, index) => (
 						<div key={index} className={classes.item} style={{transform: `translate(-${currentIndex * 100}%)`}}>
 							<img src={image} alt=""></img>
 							<span>{index+1}/{data.length-1}</span>
 						</div>
 					))}
-		</div>
-		<button onClick={toNext}>next</button>
-	</div>);
+			</div>
+			<button onClick={toNext} className={classes.next}><img src={right} alt="next"></img></button>
+		</div>);
 }
 Carousel.propTypes = {
 	index: PropTypes.string,
--- App/kasa/src/components/Dropdown/Dropdown.jsx
+++ App/kasa/src/components/Dropdown/Dropdown.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import classes from './DropDown.module.scss';
+import down from '../../assets/DDARROWFDDOWN.svg';
+import up from '../../assets/DDARROWFDUP.svg';
 
 export const DropDown = ({ title, options }) => {
 	const [open, setOpen] = useState(false);
@@ -11,7 +13,7 @@ export const DropDown = ({ title, options }) => {
     return (
       <div className={classes.container}>
         <div className={classes.title}>
-          <button onClick={handleOpen}>{title}</button>
+          <button onClick={handleOpen}>{title}<img src={open ? down : up}></img></button>
         </div>
         {open ? (
             <div className={classes.options}>
@@ -20,11 +22,12 @@ export const DropDown = ({ title, options }) => {
                 : <p>{options}</p>
                 }
             </div>
-        ): null }
+        ): null}
       </div>
     );
 }
 
 DropDown.propTypes = {
-    title: PropTypes.string
+    title: PropTypes.string,
+    oprions: Array.isArray(PropTypes.string)
 }
