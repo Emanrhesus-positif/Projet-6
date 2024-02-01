ba5fb270c001d35f061aef493f3a77089f12523d
Xeloini <Sparda042.vergil@gmail.com>
2024-02-01 21:51:46 +0100

    correction de la page d'erreur et de l'icone favoris

--- App/kasa/src/pages/PageNotFound/PageNotFound.jsx
+++ App/kasa/src/pages/PageNotFound/PageNotFound.jsx
@@ -4,12 +4,10 @@ import { Header } from '../../components/Header/Header';
 import { Footer } from '../../components/Footer/Footer';
 import { ErrorHandler } from '../../components/ErrorHandler/ErrorHandler';
 export const PageNotFound = () => {
-    const content = [
-        <main>
-            <div className={classes.headerWrap}><Header /></div>
-            <div className={classes.errorWrap}><ErrorHandler /></div>
-            <div className={classes.footerWrap}><Footer /></div>
-         </main>
-    ]
-    return content
+
+	return <main>
+		<div className={classes.headerWrap}><Header key="0" /></div>
+		<div className={classes.errorWrap}><ErrorHandler key="1" /></div>
+		<div className={classes.footerWrap}><Footer key="2" /></div>
+	</main>
 }
